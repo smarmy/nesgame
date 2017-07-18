ba6b6dedb490f512d750e71b09c8fc3e238fb526
tomas <thenetcat@gmail.com>
2017-07-18 12:04:19 +0200

    Optimize collision checks further

--- colcheck.c
+++ colcheck.c
@@ -31,10 +31,26 @@ static void __fastcall__ unlock(u8 tile_index)
   }
 }
 
+/*****************************************************************************
+ * Calculate tile index:
+ * index = (y/map_width) * map_width + (x/map_width)
+ * index = (y>>4)<<4 + (x>>4)
+ *
+ * Can be optimized as
+ *
+ * index = (y & 0xF0) + (x>>4)
+ *
+ * since left shift by 4 and then right shift by 4 just removes the bottom
+ * 4 bits.
+ *****************************************************************************/
+
+static u8 tile_index_1;
+static u8 tile_index_2;
+
 u8 __fastcall__ colcheck_down(u8 obj_index)
 {
-  u8 tile_index_1 = (((fix2i(objects_y[obj_index])+objects_bbox_y2[obj_index]+1) >> 4) << 4) + ((fix2i(objects_x[obj_index]) + objects_bbox_x1[obj_index]) >> 4);
-  u8 tile_index_2 = (((fix2i(objects_y[obj_index])+objects_bbox_y2[obj_index]+1) >> 4) << 4) + ((fix2i(objects_x[obj_index]) + objects_bbox_x2[obj_index]) >> 4);
+  tile_index_1 = ((fix2i(objects_y[obj_index])+objects_bbox_y2[obj_index]+1) & 0xF0) + ((fix2i(objects_x[obj_index]) + objects_bbox_x1[obj_index]) >> 4);
+  tile_index_2 = ((fix2i(objects_y[obj_index])+objects_bbox_y2[obj_index]+1) & 0xF0) + ((fix2i(objects_x[obj_index]) + objects_bbox_x2[obj_index]) >> 4);
 
   if (is_solid(tile_index_1) == 0) return 1;
   if (is_solid(tile_index_2) == 0) return 1;
@@ -52,8 +68,8 @@ u8 __fastcall__ colcheck_down(u8 obj_index)
 
 u8 __fastcall__ colcheck_up(u8 obj_index)
 {
-  u8 tile_index_1 = (((fix2i(objects_y[obj_index])+objects_bbox_y1[obj_index]-1) >> 4) << 4) + ((fix2i(objects_x[obj_index]) + objects_bbox_x1[obj_index]) >> 4);
-  u8 tile_index_2 = (((fix2i(objects_y[obj_index])+objects_bbox_y1[obj_index]-1) >> 4) << 4) + ((fix2i(objects_x[obj_index]) + objects_bbox_x2[obj_index]) >> 4);
+  tile_index_1 = ((fix2i(objects_y[obj_index])+objects_bbox_y1[obj_index]-1) & 0xF0) + ((fix2i(objects_x[obj_index]) + objects_bbox_x1[obj_index]) >> 4);
+  tile_index_2 = ((fix2i(objects_y[obj_index])+objects_bbox_y1[obj_index]-1) & 0xF0) + ((fix2i(objects_x[obj_index]) + objects_bbox_x2[obj_index]) >> 4);
 
   if (is_solid(tile_index_1) == 0) return 1;
   if (is_solid(tile_index_2) == 0) return 1;
@@ -63,8 +79,8 @@ u8 __fastcall__ colcheck_up(u8 obj_index)
 
 u8 __fastcall__ colcheck_right(u8 obj_index)
 {
-  u8 tile_index_1 = (((fix2i(objects_y[obj_index])+objects_bbox_y1[obj_index]+1) >> 4) << 4) + ((fix2i(objects_x[obj_index]) + objects_bbox_x2[obj_index] + 1) >> 4);
-  u8 tile_index_2 = (((fix2i(objects_y[obj_index])+objects_bbox_y2[obj_index]-1) >> 4) << 4) + ((fix2i(objects_x[obj_index]) + objects_bbox_x2[obj_index] + 1) >> 4);
+  tile_index_1 = ((fix2i(objects_y[obj_index])+objects_bbox_y1[obj_index]+1) & 0xF0) + ((fix2i(objects_x[obj_index]) + objects_bbox_x2[obj_index] + 1) >> 4);
+  tile_index_2 = ((fix2i(objects_y[obj_index])+objects_bbox_y2[obj_index]-1) & 0xF0) + ((fix2i(objects_x[obj_index]) + objects_bbox_x2[obj_index] + 1) >> 4);
 
   if (is_solid(tile_index_1) == 0) return 1;
   if (is_solid(tile_index_2) == 0) return 1;
@@ -80,8 +96,8 @@ u8 __fastcall__ colcheck_right(u8 obj_index)
 
 u8 __fastcall__ colcheck_left(u8 obj_index)
 {
-  u8 tile_index_1 = (((fix2i(objects_y[obj_index])+objects_bbox_y1[obj_index]+1) >> 4) << 4) + ((fix2i(objects_x[obj_index]) + objects_bbox_x1[obj_index] - 1) >> 4);
-  u8 tile_index_2 = (((fix2i(objects_y[obj_index])+objects_bbox_y2[obj_index]-1) >> 4) << 4) + ((fix2i(objects_x[obj_index]) + objects_bbox_x1[obj_index] - 1) >> 4);
+  tile_index_1 = ((fix2i(objects_y[obj_index])+objects_bbox_y1[obj_index]+1) & 0xF0) + ((fix2i(objects_x[obj_index]) + objects_bbox_x1[obj_index] - 1) >> 4);
+  tile_index_2 = ((fix2i(objects_y[obj_index])+objects_bbox_y2[obj_index]-1) & 0xF0) + ((fix2i(objects_x[obj_index]) + objects_bbox_x1[obj_index] - 1) >> 4);
 
   if (is_solid(tile_index_1) == 0) return 1;
   if (is_solid(tile_index_2) == 0) return 1;
@@ -97,8 +113,8 @@ u8 __fastcall__ colcheck_left(u8 obj_index)
 
 u8 __fastcall__ stairs_check(u8 obj_index)
 {
-  u8 tile_index_1 = (((fix2i(objects_y[obj_index])) >> 4) << 4) + ((fix2i(objects_x[obj_index]) + objects_bbox_x1[obj_index]) >> 4);
-  u8 tile_index_2 = (((fix2i(objects_y[obj_index])) >> 4) << 4) + ((fix2i(objects_x[obj_index]) + objects_bbox_x2[obj_index]) >> 4);
+  tile_index_1 = ((fix2i(objects_y[obj_index])) & 0xF0) + ((fix2i(objects_x[obj_index]) + objects_bbox_x1[obj_index]) >> 4);
+  tile_index_2 = ((fix2i(objects_y[obj_index])) & 0xF0) + ((fix2i(objects_x[obj_index]) + objects_bbox_x2[obj_index]) >> 4);
 
   if (tilemap[tile_index_1] == TILE_LADDER) return 1;
   if (tilemap[tile_index_2] == TILE_LADDER) return 1;
@@ -108,35 +124,11 @@ u8 __fastcall__ stairs_check(u8 obj_index)
 
 u8 __fastcall__ tile_check(u8 obj_index, u8 tile)
 {
-  u8 tile_index_1 = (((fix2i(objects_y[obj_index])+2) >> 4) << 4) + ((fix2i(objects_x[obj_index]) + objects_bbox_x1[obj_index]) >> 4);
-  u8 tile_index_2 = (((fix2i(objects_y[obj_index])+15) >> 4) << 4) + ((fix2i(objects_x[obj_index]) + objects_bbox_x2[obj_index]) >> 4);
+  tile_index_1 = ((fix2i(objects_y[obj_index])+2) & 0xF0) + ((fix2i(objects_x[obj_index]) + objects_bbox_x1[obj_index]) >> 4);
+  tile_index_2 = ((fix2i(objects_y[obj_index])+15) & 0xF0) + ((fix2i(objects_x[obj_index]) + objects_bbox_x2[obj_index]) >> 4);
 
   if (tilemap[tile_index_1] == tile) return tile_index_1;
   if (tilemap[tile_index_2] == tile) return tile_index_2;
 
   return 0;
 }
-
-/*u8 __fastcall__ colcheck_objects(u8 obj_index_1, u8 obj_index_2)
-{
-  static u8 o1x, o1y;
-  static u8 o2x, o2y;
-  static u8 o1w, o1h;
-  static u8 o2w, o2h;
-
-  o1x = fix2i(objects_x[obj_index_1]) + objects_bbox_x1[obj_index_1];
-  o1y = fix2i(objects_y[obj_index_1]) + objects_bbox_y1[obj_index_1];
-  o2x = fix2i(objects_x[obj_index_2]) + objects_bbox_x1[obj_index_2];
-  o2y = fix2i(objects_y[obj_index_2]) + objects_bbox_y1[obj_index_2];
-  o1w = objects_bbox_x2[obj_index_1] - objects_bbox_x1[obj_index_1];
-  o1h = objects_bbox_y2[obj_index_1] - objects_bbox_y1[obj_index_1];
-  o2w = objects_bbox_x2[obj_index_2] - objects_bbox_x1[obj_index_2];
-  o2h = objects_bbox_y2[obj_index_2] - objects_bbox_y1[obj_index_2];
-
-  if ((o1x + o1w) < o2x) return 0;
-  if ((o1y + o1h) < o2y) return 0;
-  if ((o2x + o2w) < o1x) return 0;
-  if ((o2y + o2h) < o1y) return 0;
-
-  return 1;
-}*/
